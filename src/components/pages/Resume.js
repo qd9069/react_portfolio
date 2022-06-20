67c1d69ad1c0bda100b8cf40176953d7b258e38a
Elaine Deng <qd9069@gmail.com>
2022-06-20 00:46:46 -0700

    Modified list logic in resume page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -4,35 +4,101 @@ import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 
 export default function Resume() {
+  const fESkills = [
+    {
+      id: 1,
+      skill: "HTML",
+    },
+    {
+      id: 2,
+      skill: "CSS",
+    },
+    {
+      id: 3,
+      skill: "JavaScript",
+    },
+    {
+      id: 4,
+      skill: "jQuery",
+    },
+    {
+      id: 5,
+      skill: "Responsive design",
+    },
+    {
+      id: 6,
+      skill: "React",
+    },
+    {
+      id: 7,
+      skill: "Bootstrap",
+    },
+  ];
+
+  const bESkills = [
+    {
+      id: 1,
+      skill: "APIs",
+    },
+    {
+      id: 2,
+      skill: "Node",
+    },
+    {
+      id: 3,
+      skill: "Express",
+    },
+    {
+      id: 4,
+      skill: "MySQL, Sequelize",
+    },
+    {
+      id: 5,
+      skill: "MongoDB, Mongoose",
+    },
+    {
+      id: 6,
+      skill: "REST",
+    },
+    {
+      id: 7,
+      skill: "GraphQL",
+    },
+  ];
+
   return (
     <React.Fragment>
       <CssBaseline />
-      <Container maxWidth="lg" sx={{my: 7}}>
-        <Box sx={{ bgcolor: '#DBEFF6', padding: "3px 20px", borderRadius: '10px', opacity: 0.95 }} >
-        <div>
-           <h1>Resume</h1>
-           <p>Download my <a href="https://drive.google.com/file/d/1x1ZnnYs4oQoSCHDXF8Zw15VyTKrJOAXI/view?usp=sharing">resume</a></p>
-          <h2>Front-end Proficiencies</h2>
-          <ul>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>JavaScript</li>
-            <li>jQuery</li>
-            <li>Responsive design</li>
-            <li>React</li>
-            <li>Bootstrap</li>
-          </ul>
-          <h2>Back-end Proficiencies</h2>
-          <ul>
-            <li>APIs</li>
-            <li>Node</li>
-            <li>Express</li>
-            <li>MySQL, Sequelize</li>
-            <li>MongoDB, Mongoose</li>
-            <li>REST</li>
-            <li>GraphQL</li>
-          </ul>
-        </div>
+      <Container maxWidth="lg" sx={{ my: 7 }}>
+        <Box
+          sx={{
+            bgcolor: "#DBEFF6",
+            padding: "3px 20px",
+            borderRadius: "10px",
+            opacity: 0.95,
+          }}
+        >
+          <div>
+            <h1>Resume</h1>
+            <p>
+              Download my{" "}
+              <a href="https://drive.google.com/file/d/1x1ZnnYs4oQoSCHDXF8Zw15VyTKrJOAXI/view?usp=sharing">
+                resume
+              </a>
+            </p>
+            <h2>Front-end Proficiencies</h2>
+            <ul>
+              {fESkills.map(({ id, skill }) => (
+                <li key={id}>{skill}</li>
+              ))}
+            </ul>
+            <h2>Back-end Proficiencies</h2>
+            <ul>
+              {bESkills.map(({ id, skill }) => (
+                <li key={id}>{skill}</li>
+              ))}
+            </ul>
+          </div>
         </Box>
       </Container>
     </React.Fragment>

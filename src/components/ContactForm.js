be064898b687879d02f8d26b9dfedfd10bd24466
Elaine Deng <qd9069@gmail.com>
2022-06-21 02:51:08 -0700

    Added logic for emailjs

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { send } from "emailjs-com";
+import Swal from "sweetalert2";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import TextField from "@mui/material/TextField";
@@ -5,7 +8,34 @@ import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 
+const initialFormState = {
+    senderName: "",
+    senderEmail: "",
+    message: "",
+  };
+
 export default function ContactForm() {
+    const [formState, setFormState] = useState(initialFormState);
+
+    const handleInputChange = (evt) => {
+        const { name, value } = evt.target;
+        setFormState((prevState) => ({ ...prevState, [name]: value }));
+    };
+
+    const handleSubmit = async (evt) => {
+        evt.preventDefault();
+        console.log(formState);
+    
+        
+    
+        setFormState({
+            senderName: "",
+            senderEmail: "",
+            message: "",
+        });
+    
+    };
+
     return (
         <Card
             style={{ maxWidth: 650, padding: "20px 5px", borderRadius: "16px" }}
@@ -15,7 +45,7 @@ export default function ContactForm() {
               <Typography gutterBottom variant="h5">
                 Contact Me
               </Typography>
-              <form>
+              <form onSubmit={handleSubmit}>
                 <Grid container spacing={1}>
                   <Grid xs={12} item>
                     <Typography
@@ -31,6 +61,9 @@ export default function ContactForm() {
                       placeholder="Enter your name"
                       fullWidth
                       required
+                      name="senderName"
+                      value={formState.senderName}
+                      onChange={handleInputChange}
                     />
                   </Grid>
                   <Grid xs={12} item>
@@ -48,6 +81,9 @@ export default function ContactForm() {
                       fullWidth
                       required
                       type="email"
+                      name="senderEmail"
+                      value={formState.senderEmail}
+                      onChange={handleInputChange}
                     />
                   </Grid>
                   <Grid xs={12} item>
@@ -66,6 +102,9 @@ export default function ContactForm() {
                       required
                       multiline
                       rows={10}
+                      name="message"
+                      value={formState.message}
+                      onChange={handleInputChange}
                     />
                   </Grid>
                   <Grid xs={12} item>

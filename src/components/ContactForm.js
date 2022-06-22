16c54655dacde2c59cb01fba81775508126c87a1
Elaine Deng <qd9069@gmail.com>
2022-06-22 00:16:31 -0700

    Added logic for emailjs with .env file

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -8,118 +8,150 @@ import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 
+// Environment variables: UserID has been renamed to Public Key
+const serviceID = process.env.REACT_APP_SERVICE_ID;
+const templateID = process.env.REACT_APP_TEMPLATE_ID;
+const userID = process.env.REACT_APP_USER_ID;
+
 const initialFormState = {
-    senderName: "",
-    senderEmail: "",
-    message: "",
-  };
+  senderName: "",
+  senderEmail: "",
+  message: "",
+};
 
 export default function ContactForm() {
-    const [formState, setFormState] = useState(initialFormState);
+  const [formState, setFormState] = useState(initialFormState);
 
-    const handleInputChange = (evt) => {
-        const { name, value } = evt.target;
-        setFormState((prevState) => ({ ...prevState, [name]: value }));
-    };
+  const handleInputChange = (evt) => {
+    const { name, value } = evt.target;
+    setFormState((prevState) => ({ ...prevState, [name]: value }));
+  };
 
-    const handleSubmit = async (evt) => {
-        evt.preventDefault();
-        console.log(formState);
-    
-        
-    
-        setFormState({
-            senderName: "",
-            senderEmail: "",
-            message: "",
+  const handleSubmit = async (evt) => {
+    evt.preventDefault();
+    // console.log(formState);
+
+    // console.log('serviceId: ' + serviceID);
+    // console.log('templateId: ' + templateID);
+    // console.log('userId: ' + userID);
+    send(serviceID, templateID, { formState }, userID)
+      .then((result) => {
+        // console.log(result.text);
+        Swal.fire({
+          icon: "success",
+          title: "Message Sent Successfully",
+          backdrop: `
+            rgba(0,0,123,0.4)
+            url("https://sweetalert2.github.io/images/nyan-cat.gif")
+            left top
+            no-repeat
+            `,
         });
-    
-    };
+      })
+      .catch((error) => {
+        // console.log(error.text);
+        Swal.fire({
+          icon: "error",
+          title: "Oops, something went wrong",
+          text: error.text,
+          backdrop: `
+            rgba(0,0,123,0.4)
+            `,
+        });
+      });
 
-    return (
-        <Card
-            style={{ maxWidth: 650, padding: "20px 5px", borderRadius: "16px" }}
-            sx={{ my: 4, ml: 9 }}
-          >
-            <CardContent>
-              <Typography gutterBottom variant="h5">
-                Contact Me
+    // clear the input after submitting the form
+    setFormState({
+      senderName: "",
+      senderEmail: "",
+      message: "",
+    });
+  };
+
+  return (
+    <Card
+      style={{ maxWidth: 650, padding: "20px 5px", borderRadius: "16px" }}
+      sx={{ my: 4, ml: 9 }}
+    >
+      <CardContent>
+        <Typography gutterBottom variant="h5">
+          Contact Me
+        </Typography>
+        <form onSubmit={handleSubmit}>
+          <Grid container spacing={1}>
+            <Grid xs={12} item>
+              <Typography
+                gutterBottom
+                variant="body2"
+                component="p"
+                color="textSecondary"
+              >
+                Name:
               </Typography>
-              <form onSubmit={handleSubmit}>
-                <Grid container spacing={1}>
-                  <Grid xs={12} item>
-                    <Typography
-                      gutterBottom
-                      variant="body2"
-                      component="p"
-                      color="textSecondary"
-                    >
-                      Name:
-                    </Typography>
-                    <TextField
-                      // label="Name"
-                      placeholder="Enter your name"
-                      fullWidth
-                      required
-                      name="senderName"
-                      value={formState.senderName}
-                      onChange={handleInputChange}
-                    />
-                  </Grid>
-                  <Grid xs={12} item>
-                    <Typography
-                      gutterBottom
-                      variant="body2"
-                      component="p"
-                      color="textSecondary"
-                    >
-                      Email address:
-                    </Typography>
-                    <TextField
-                      // label="Email address"
-                      placeholder="Enter email"
-                      fullWidth
-                      required
-                      type="email"
-                      name="senderEmail"
-                      value={formState.senderEmail}
-                      onChange={handleInputChange}
-                    />
-                  </Grid>
-                  <Grid xs={12} item>
-                    <Typography
-                      gutterBottom
-                      variant="body2"
-                      component="p"
-                      color="textSecondary"
-                    >
-                      Message:
-                    </Typography>
-                    <TextField
-                      // label="Message"
-                      placeholder="Type your message here"
-                      fullWidth
-                      required
-                      multiline
-                      rows={10}
-                      name="message"
-                      value={formState.message}
-                      onChange={handleInputChange}
-                    />
-                  </Grid>
-                  <Grid xs={12} item>
-                    <Button
-                      size="small"
-                      variant="contained"
-                      color="primary"
-                      type="submit"
-                    >
-                      Submit
-                    </Button>
-                  </Grid>
-                </Grid>
-              </form>
-            </CardContent>
-          </Card>
-    );
+              <TextField
+                // label="Name"
+                placeholder="Enter your name"
+                fullWidth
+                required
+                name="senderName"
+                value={formState.senderName}
+                onChange={handleInputChange}
+              />
+            </Grid>
+            <Grid xs={12} item>
+              <Typography
+                gutterBottom
+                variant="body2"
+                component="p"
+                color="textSecondary"
+              >
+                Email address:
+              </Typography>
+              <TextField
+                // label="Email address"
+                placeholder="Enter email"
+                fullWidth
+                required
+                type="email"
+                name="senderEmail"
+                value={formState.senderEmail}
+                onChange={handleInputChange}
+              />
+            </Grid>
+            <Grid xs={12} item>
+              <Typography
+                gutterBottom
+                variant="body2"
+                component="p"
+                color="textSecondary"
+              >
+                Message:
+              </Typography>
+              <TextField
+                // label="Message"
+                placeholder="Type your message here"
+                fullWidth
+                required
+                multiline
+                rows={10}
+                name="message"
+                value={formState.message}
+                onChange={handleInputChange}
+              />
+            </Grid>
+            <Grid xs={12} item>
+              <Button
+                size="small"
+                variant="contained"
+                color="primary"
+                type="submit"
+              >
+                Submit
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </CardContent>
+    </Card>
+  );
 }

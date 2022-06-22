04c038066d48ea788689280a14173d2e889d9af0
Elaine Deng <qd9069@gmail.com>
2022-06-22 01:16:04 -0700

    Centered the contact me form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -71,89 +71,98 @@ export default function ContactForm() {
   };
 
   return (
-    <Card
-      style={{ maxWidth: 650, padding: "20px 5px", borderRadius: "16px" }}
-      sx={{ my: 4, ml: 9 }}
+    <Grid
+      container
+      justifyContent="center"
+      alignItems="center"
+      direction="column"
+      // style={{ minHeight: "100vh" }}
+      sx={{ mt: 3, mb: 6 }}
     >
-      <CardContent>
-        <Typography gutterBottom variant="h5">
-          Contact Me
-        </Typography>
-        <form onSubmit={handleSubmit}>
-          <Grid container spacing={1}>
-            <Grid xs={12} item>
-              <Typography
-                gutterBottom
-                variant="body2"
-                component="p"
-                color="textSecondary"
-              >
-                Name:
-              </Typography>
-              <TextField
-                // label="Name"
-                placeholder="Enter your name"
-                fullWidth
-                required
-                name="senderName"
-                value={formState.senderName}
-                onChange={handleInputChange}
-              />
+      <Card
+        style={{ maxWidth: 650, padding: "20px 5px", borderRadius: "16px" }}
+        // sx={{ my: 4, ml: 9 }}
+      >
+        <CardContent>
+          <Typography gutterBottom variant="h5">
+            Contact Me
+          </Typography>
+          <form onSubmit={handleSubmit}>
+            <Grid container spacing={1}>
+              <Grid xs={12} item>
+                <Typography
+                  gutterBottom
+                  variant="body2"
+                  component="p"
+                  color="textSecondary"
+                >
+                  Name:
+                </Typography>
+                <TextField
+                  // label="Name"
+                  placeholder="Enter your name"
+                  fullWidth
+                  required
+                  name="senderName"
+                  value={formState.senderName}
+                  onChange={handleInputChange}
+                />
+              </Grid>
+              <Grid xs={12} item>
+                <Typography
+                  gutterBottom
+                  variant="body2"
+                  component="p"
+                  color="textSecondary"
+                >
+                  Email address:
+                </Typography>
+                <TextField
+                  // label="Email address"
+                  placeholder="Enter email"
+                  fullWidth
+                  required
+                  type="email"
+                  name="senderEmail"
+                  value={formState.senderEmail}
+                  onChange={handleInputChange}
+                />
+              </Grid>
+              <Grid xs={12} item>
+                <Typography
+                  gutterBottom
+                  variant="body2"
+                  component="p"
+                  color="textSecondary"
+                >
+                  Message:
+                </Typography>
+                <TextField
+                  // label="Message"
+                  placeholder="Type your message here"
+                  fullWidth
+                  required
+                  multiline
+                  rows={10}
+                  name="message"
+                  value={formState.message}
+                  onChange={handleInputChange}
+                />
+              </Grid>
+              <Grid xs={12} item>
+                <Button
+                  size="small"
+                  variant="contained"
+                  color="primary"
+                  type="submit"
+                >
+                  Submit
+                </Button>
+              </Grid>
             </Grid>
-            <Grid xs={12} item>
-              <Typography
-                gutterBottom
-                variant="body2"
-                component="p"
-                color="textSecondary"
-              >
-                Email address:
-              </Typography>
-              <TextField
-                // label="Email address"
-                placeholder="Enter email"
-                fullWidth
-                required
-                type="email"
-                name="senderEmail"
-                value={formState.senderEmail}
-                onChange={handleInputChange}
-              />
-            </Grid>
-            <Grid xs={12} item>
-              <Typography
-                gutterBottom
-                variant="body2"
-                component="p"
-                color="textSecondary"
-              >
-                Message:
-              </Typography>
-              <TextField
-                // label="Message"
-                placeholder="Type your message here"
-                fullWidth
-                required
-                multiline
-                rows={10}
-                name="message"
-                value={formState.message}
-                onChange={handleInputChange}
-              />
-            </Grid>
-            <Grid xs={12} item>
-              <Button
-                size="small"
-                variant="contained"
-                color="primary"
-                type="submit"
-              >
-                Submit
-              </Button>
-            </Grid>
-          </Grid>
-        </form>
-      </CardContent>
-    </Card>
+          </form>
+        </CardContent>
+      </Card>
+    </Grid>
   );
 }

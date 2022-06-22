8ed674b610f714014986d211a2c897378832aef7
Elaine Deng <qd9069@gmail.com>
2022-06-22 02:47:25 -0700

    Added scrollable section in contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -12,7 +12,7 @@ import AccordionDetails from "@mui/material/AccordionDetails";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import Alert from "@mui/material/Alert";
 import ContactMailIcon from "@mui/icons-material/ContactMail";
-import ContactForm from '../ContactForm';
+import ContactForm from "../ContactForm";
 
 export default function Contact() {
   return (
@@ -28,60 +28,69 @@ export default function Contact() {
           }}
         >
           <h1>Contact</h1>
-          <ContactForm />
-          <div>
-            <Alert
-              icon={<ContactMailIcon />}
-              severity="success"
-              sx={{ fontWeight: "bold", fontSize: "17px" }}
-            >
-              {" "}
-              Additional contact information provided below{" "}
-            </Alert>
-            <Accordion style={{ borderRadius: "6px" }} sx={{ my: 1 }}>
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1a-content"
-                id="panel1a-header"
+          <div
+            style={{
+              maxHeight: "79vh",
+              overflow: "auto",
+            }}
+          >
+            <ContactForm />
+            <div>
+              <Alert
+                icon={<ContactMailIcon />}
+                severity="success"
+                // sx={{ fontWeight: "bold", fontSize: "17px" }}
+                sx={{ fontSize: "17px" }}
               >
-                <Typography>Phone</Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Typography>
-                  Feel free to call or text:{" "}
-                  <IconButton
-                    color="info"
-                    aria-label="Phone"
-                    href={`tel:8582678234`}
-                    size="small"
-                  >
-                    <PhoneIphoneIcon /> (858) 267 - 8234
-                  </IconButton>
-                </Typography>
-              </AccordionDetails>
-            </Accordion>
-            <Accordion style={{ borderRadius: "6px" }} sx={{ mt: 1, mb: 5 }}>
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel2a-content"
-                id="panel2a-header"
-              >
-                <Typography>Email</Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Typography>
-                  Please send email to:{" "}
-                  <IconButton
-                    color="info"
-                    aria-label="Email"
-                    href={`mailto:qdengliao@gmail.com`}
-                    size="small"
-                  >
-                    <AttachEmailIcon /> qdengliao@gmail.com
-                  </IconButton>
-                </Typography>
-              </AccordionDetails>
-            </Accordion>
+                {" "}
+                <strong>Additional contact information provided below</strong> -
+                scroll down to view more{" "}
+              </Alert>
+              <Accordion style={{ borderRadius: "6px" }} sx={{ my: 1 }}>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                  <Typography>Phone</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>
+                    Feel free to call or text:{" "}
+                    <IconButton
+                      color="info"
+                      aria-label="Phone"
+                      href={`tel:8582678234`}
+                      size="small"
+                    >
+                      <PhoneIphoneIcon /> (858) 267 - 8234
+                    </IconButton>
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
+              <Accordion style={{ borderRadius: "6px" }} sx={{ mt: 1, mb: 5 }}>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel2a-content"
+                  id="panel2a-header"
+                >
+                  <Typography>Email</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>
+                    Please send email to:{" "}
+                    <IconButton
+                      color="info"
+                      aria-label="Email"
+                      href={`mailto:qdengliao@gmail.com`}
+                      size="small"
+                    >
+                      <AttachEmailIcon /> qdengliao@gmail.com
+                    </IconButton>
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
+            </div>
           </div>
         </Box>
       </Container>
